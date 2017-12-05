ec0cee222787230fd7b7163018e5028ecf2a2328
NowUI <nowui@163.com>
2017-12-06 01:16:31 +0800

    修改

---
+++ server-eureka/src/main/java/com/nowui/cloud/eureka/ServerEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.nowui.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerEurekaApplication.class, args);
+    }
+
+}
---
+++ server-zuul/src/main/java/com/nowui/cloud/zuul/ServerZuulApplication.java
@@ -0,0 +1,17 @@
+package com.nowui.cloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableZuulProxy
+public class ServerZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerZuulApplication.class, args);
+    }
+
+}
