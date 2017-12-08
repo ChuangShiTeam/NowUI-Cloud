06f1c0d30082af107f575ed8995118bb24f726a0
NowUI <nowui@163.com>
2017-12-09 03:20:50 +0800

    修改

--- module-shop/api/src/main/java/com/nowui/cloud/shop/product/mapper/ProductMapper.java
+++ module-shop/api/src/main/java/com/nowui/cloud/shop/product/mapper/ProductMapper.java
@@ -2,12 +2,14 @@ package com.nowui.cloud.shop.product.mapper;
 
 import com.nowui.cloud.mapper.BaseMapper;
 import com.nowui.cloud.shop.product.entity.Product;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Component;
 
 /**
  * @author ZhongYongQiang
  */
-@Component(value = "productMapper")
+@Mapper
+@Component
 public interface ProductMapper extends BaseMapper<Product> {
 
 }
--- module-shop/api/src/main/java/com/nowui/cloud/shop/product/service/impl/ProductServiceImpl.java
+++ module-shop/api/src/main/java/com/nowui/cloud/shop/product/service/impl/ProductServiceImpl.java
@@ -1,12 +1,13 @@
 package com.nowui.cloud.shop.product.service.impl;
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
 import com.nowui.cloud.shop.product.entity.Product;
 import com.nowui.cloud.shop.product.mapper.ProductMapper;
 import com.nowui.cloud.shop.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,7 +21,10 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public List<Product> adminList() {
-        List<Product> productList = new ArrayList<Product>();
+        List<Product> productList = productMapper.selectPage(
+                new Page<Product>(1, 10),
+                new EntityWrapper<Product>()
+        );
         return productList;
     }
 
