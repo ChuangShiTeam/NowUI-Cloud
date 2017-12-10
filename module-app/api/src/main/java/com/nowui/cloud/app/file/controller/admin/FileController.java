b4143cfff2d45c6858a648457e06c4ff24445764
NowUI <nowui@163.com>
2017-12-10 12:37:20 +0800

    修改

---
+++ module-app/api/src/main/java/com/nowui/cloud/app/file/controller/admin/FileController.java
@@ -0,0 +1,13 @@
+package com.nowui.cloud.shop.pay.controller.admin;
+
+import com.nowui.cloud.controller.BaseController;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author ZhongYongQiang
+ */
+@RestController
+public class FileController extends BaseController {
+
+
+}
