43c3af484db8f9d4c6ee99897c1f45624edc3744
shawn <xx090304@163.com>
2017-12-11 22:54:56 +0800

    包裹接口返回结果

--- module-cms/module-cms-api/src/main/java/com/nowui/cloud/cms/banner/controller/admin/BannerController.java
+++ module-cms/module-cms-api/src/main/java/com/nowui/cloud/cms/banner/controller/admin/BannerController.java
@@ -9,24 +9,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
+import java.util.Map;
+
 @RestController
 public class BannerController extends BaseController {
-
     @Autowired
     private BannerService bannerService;
 
     @ApiOperation(value = "广告查询列表")
     @RequestMapping(value = "/banner/list", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-
-    List<Banner> home(String appId,String title,Integer pageIndex){
+    public Map<String, Object> home(String appId,String title,Integer pageIndex) {
         validateRequest();
         Integer pageSize = 10;
-        List<Banner> resultList = bannerService.Query(appId,title,pageIndex,pageSize);
+        Integer resultTotal = 0;
+        List<Banner> resultList = bannerService.Query(appId, title, pageIndex, pageSize);
         validateResponse();
-        return resultList;
+        return renderJson(resultTotal, resultList);
     }
 }
-
-//    List<Banner> home(@RequestBody String body){
