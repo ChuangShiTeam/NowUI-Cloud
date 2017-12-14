06623b0277dfe0ed4658ac4a30ad7711e13986af
NowUI <nowui@163.com>
2017-12-14 16:24:43 +0800

    修改

---
+++ server-zuul/src/main/java/com/nowui/cloud/zuul/filter/RequestFilter.java
@@ -0,0 +1,73 @@
+package com.nowui.cloud.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ * @author ZhongYongQiang
+ */
+@Component
+public class RequestFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+
+        String body = readData(request);
+
+        System.out.println(body);
+
+        return null;
+    }
+
+    public static String readData(HttpServletRequest request) {
+        BufferedReader br = null;
+
+        try {
+            StringBuilder result = new StringBuilder();
+
+            String line;
+            for (br = request.getReader(); (line = br.readLine()) != null; result.append(line)) {
+                if (result.length() > 0) {
+                    result.append("\n");
+                }
+            }
+
+            line = result.toString();
+            return line;
+        } catch (IOException var12) {
+            throw new RuntimeException(var12);
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException var11) {
+
+                }
+            }
+
+        }
+    }
+}
---
+++ server-zuul/src/main/java/com/nowui/cloud/zuul/filter/ResponseFilter.java
@@ -0,0 +1,59 @@
+package com.nowui.cloud.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import static org.springframework.util.ReflectionUtils.rethrowRuntimeException;
+
+/**
+ * @author ZhongYongQiang
+ */
+@Component
+public class ResponseFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        try {
+            RequestContext requestContext = RequestContext.getCurrentContext();
+            InputStream stream = requestContext.getResponseDataStream();
+            String body = StreamUtils.copyToString(stream, Charset.forName("UTF-8"));
+
+            if (StringUtils.isNotBlank(body)) {
+
+            }
+
+            System.out.println(body);
+
+            requestContext.setResponseBody(body);
+        } catch (IOException e) {
+            rethrowRuntimeException(e);
+        }
+
+        return null;
+    }
+
+}
