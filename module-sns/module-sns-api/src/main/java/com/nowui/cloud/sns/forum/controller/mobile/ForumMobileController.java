3add3babee95e224fa257edc16bb7efe91691416
xupengfei111 <15303933327@163.com>
2018-01-08 21:35:53 +0800

    1,创建论坛接口 2,查看论坛信息接口 3,论坛查看全部用户接口 以下未完善 4,论坛头像更新接口 5,论坛名称更新接口 6,论坛简介更新接口 7,论坛删除接口

--- module-sns/module-sns-api/src/main/java/com/nowui/cloud/sns/forum/controller/mobile/ForumMobileController.java
+++ module-sns/module-sns-api/src/main/java/com/nowui/cloud/sns/forum/controller/mobile/ForumMobileController.java
@@ -4,6 +4,7 @@ import com.nowui.cloud.base.file.entity.enums.FileType;
 import com.nowui.cloud.controller.BaseController;
 import com.nowui.cloud.sns.forum.entity.Forum;
 import com.nowui.cloud.sns.forum.service.ForumService;
+import com.nowui.cloud.sns.forum.service.TopicForumService;
 import com.nowui.cloud.util.Util;
 
 import io.swagger.annotations.Api;
@@ -32,6 +33,9 @@ public class ForumMobileController extends BaseController {
 	
 	 @Autowired
 	 private ForumService forumService;
+	 
+	 @Autowired
+	 private TopicForumService topicForumServicepic;
 
 	 @ApiOperation(value = "新增论坛信息")
 	 @RequestMapping(value = "/forum/mobile/save", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -81,9 +85,10 @@ public class ForumMobileController extends BaseController {
         );
 
         Forum result = forumService.find(body.getForumId());
-        //根据forumModerator查询版主信息:去会员表查找--昵称,个人简介,头像封装成json
+        //根据forumModerator查询版主信息:去会员表查找--昵称,个人简介,头像,userId封装成json
         
-        //根据论坛id去forum_user_follow_map表查找此论坛的userId,然后遍历查找用户头像
+        //根据论坛id去forum_user_follow_map表查找此论坛的userId,然后遍历查找用户头像,只返回前6个的头像和userId.
+        
         
         
         validateResponse(
@@ -107,8 +112,108 @@ public class ForumMobileController extends BaseController {
 
         return renderJson(result);
     }
-	 
 	
+	@ApiOperation(value = "论坛信息")
+    @RequestMapping(value = "/forum/mobile/findAll", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> findAll(@RequestBody Forum body) {
+        validateRequest(
+                body,
+                Forum.APP_ID,
+                Forum.FORUM_ID
+        );
 
+        Forum result = forumService.find(body.getForumId());
+        
+        //根据论坛id去forum_user_follow_map表查找此论坛的userId,然后遍历查找用户头像,返回全部的的头像和userId.
+        
+        
+        
+        validateResponse(
+//                Forum.FORUM_ID,
+                Forum.FORUM_MEDIA_ID,
+//                Forum.FORUM_MEDIA_TYPE,
+//                Forum.FORUM_BACKGROUND_MEDIA_ID,
+//                Forum.FORUM_BACKGROUND_MEDIA_TYPE,
+                Forum.FORUM_NAME,
+                Forum.FORUM_DESCRIPTION,
+                Forum.FORUM_MODERATOR
+//                Forum.FORUM_TOPIC_LOCATION,
+//                Forum.FORUM_SORT,
+//                Forum.FORUM_TOP,
+//                Forum.FORUM_TOP_LEVEL,
+//                Forum.FORUM_TOP_END_TIME,
+//                Forum.FORUM_IS_ACTIVE,
+//                Forum.FORUM_IS_FOLLOW,
+//                Forum.FORUM_IS_RECOMAND
+        );
+
+        return renderJson(result);
+    }
+
+	@ApiOperation(value = "更新论坛头像")
+	@RequestMapping(value = "/forum/mobile/update/media", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, Object> updateMedia(@RequestBody Forum body) {
+	    validateRequest(
+          body,
+          Forum.APP_ID,
+          Forum.FORUM_ID,
+          Forum.FORUM_MEDIA_ID
+       );
+	   //不清楚是否单独写一个更改背景图片的接口
+	   body.setForumBackgroundMediaId(body.getForumMediaId());
+
+       Boolean result = forumService.update(body, body.getForumId(), body.getSystemRequestUserId(), body.getSystemVersion());
+
+       return renderJson(result);
+    }
+	
+	@ApiOperation(value = "更新论坛名称")
+	@RequestMapping(value = "/forum/mobile/update/name", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, Object> updateName(@RequestBody Forum body) {
+	    validateRequest(
+          body,
+          Forum.APP_ID,
+          Forum.FORUM_ID,
+          Forum.FORUM_NAME
+       );
+
+       Boolean result = forumService.update(body, body.getForumId(), body.getSystemRequestUserId(), body.getSystemVersion());
+
+       return renderJson(result);
+    }
+	
+	@ApiOperation(value = "更新论坛简介")
+	@RequestMapping(value = "/forum/mobile/update/description", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, Object> updateDescription(@RequestBody Forum body) {
+	    validateRequest(
+          body,
+          Forum.APP_ID,
+          Forum.FORUM_ID,
+          Forum.FORUM_DESCRIPTION
+       );
+
+       Boolean result = forumService.update(body, body.getForumId(), body.getSystemRequestUserId(), body.getSystemVersion());
+
+       return renderJson(result);
+    }
+	
+	@ApiOperation(value = "删除论坛")
+    @RequestMapping(value = "/forum/admin/delete", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> delete(@RequestBody Forum body) {
+        validateRequest(
+                body,
+                Forum.FORUM_ID,
+                Forum.APP_ID,
+                Forum.SYSTEM_VERSION
+        );
+        //先从论坛信息表删除
+        Boolean result = forumService.delete(body.getForumId(), body.getSystemRequestUserId(), body.getSystemVersion());
+        //再从论坛动态表中逻辑删除所有的有论坛id的字段
+    	if (result) {
+    		Boolean delResult = topicForumServicepic.deleteByForumId(body.getAppId(), body.getForumId(), body.getSystemRequestUserId(), body.getSystemVersion());
+		}
+        
+        return renderJson(result);
+    }
 
 }
