7834a5779a2cfdb159711955e42862d4bf143a13
NowUI <nowui@163.com>
2018-01-09 15:27:04 +0800

    排除eureka、zuul启动时对elasticsearch、rabbit、redis的依赖

--- server-eureka/src/main/java/com/nowui/cloud/eureka/ServerEurekaApplication.java
+++ server-eureka/src/main/java/com/nowui/cloud/eureka/ServerEurekaApplication.java
@@ -1,11 +1,17 @@
 package com.nowui.cloud.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * @author ZhongYongQiang
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, ElasticsearchAutoConfiguration.class, RabbitAutoConfiguration.class, RedisAutoConfiguration.class})
 @EnableEurekaServer
 public class ServerEurekaApplication {
     public static void main(String[] args) {
