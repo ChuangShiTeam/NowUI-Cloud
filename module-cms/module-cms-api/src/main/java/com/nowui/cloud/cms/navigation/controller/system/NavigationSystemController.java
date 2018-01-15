8783009c0bdd9b88e6b7b56d476b8401b7dac137
marcus <marcus_hualebin@163.com>
2018-01-15 22:06:50 +0800

    完善

--- module-cms/module-cms-api/src/main/java/com/nowui/cloud/cms/navigation/controller/system/NavigationSystemController.java
+++ module-cms/module-cms-api/src/main/java/com/nowui/cloud/cms/navigation/controller/system/NavigationSystemController.java
@@ -1,13 +1,18 @@
 package com.nowui.cloud.cms.navigation.controller.system;
 
-import com.alibaba.fastjson.JSON;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nowui.cloud.base.file.entity.File;
+import com.nowui.cloud.base.file.rpc.FileRpc;
 import com.nowui.cloud.cms.navigation.entity.Navigation;
 import com.nowui.cloud.cms.navigation.rpc.NavigationRpc;
 import com.nowui.cloud.cms.navigation.service.NavigationService;
+import com.nowui.cloud.util.Util;
+
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 导航栏系统端控制器
@@ -19,5 +24,29 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(value = "导航栏", description = "导航栏系统端接口管理")
 @RestController
 public class NavigationSystemController implements NavigationRpc {
+    
+    @Autowired
+    private NavigationService navigationService;
+    
+    @Autowired
+    private FileRpc fileRpc;
+
+    @Override
+    public List<Navigation> listByCategoryCodeV1(String appId, String navigationCategoryCode) {
+        
+        List<Navigation> navigationList = navigationService.listByCategoryCode(appId, navigationCategoryCode);
+       
+        if (navigationList == null || navigationList.size() == 0) {
+            return null;
+        }
+        
+        String fileIds = Util.beanToFieldString(navigationList, Navigation.NAVIGATION_IMAGE);
+        
+        List<File> fileList = fileRpc.findsV1(fileIds);
+        
+        navigationList = Util.beanAddField(navigationList, Navigation.NAVIGATION_IMAGE, fileList, File.FILE_PATH);
+        
+        return navigationList;
+    }
 
 }
--- module-cms/module-cms-sys/src/main/java/com/nowui/cloud/cms/navigation/rpc/NavigationRpc.java
+++ module-cms/module-cms-sys/src/main/java/com/nowui/cloud/cms/navigation/rpc/NavigationRpc.java
@@ -1,7 +1,14 @@
 package com.nowui.cloud.cms.navigation.rpc;
 
+import java.util.List;
+
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.nowui.cloud.cms.navigation.entity.Navigation;
 
 /**
  * 导航栏服务调用
@@ -13,5 +20,18 @@ import org.springframework.stereotype.Component;
 @Component(value = "navigationRpc")
 @FeignClient(name = "module-cms")
 public interface NavigationRpc {
+    
+    /**
+     * 根据导航栏分类编码查询导航栏列表信息
+     * 
+     * @param appId 应用编号
+     * @param navigationCategoryCode 导航栏分类编码
+     * @return List<Navigation> 导航栏信息列表
+     */
+    @RequestMapping(value = "/navigation/system/v1/list/by/category/code", method = RequestMethod.POST)
+    List<Navigation> listByCategoryCodeV1(
+            @RequestParam(value = "appId", required = true) String appId,
+            @RequestParam(value = "navigationCategoryCode", required = true) String navigationCategoryCode
+    );
 
 }
