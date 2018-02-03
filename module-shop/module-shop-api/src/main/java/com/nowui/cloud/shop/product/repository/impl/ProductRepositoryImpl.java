f8dd9c8d866659e9119d89efbc7f10bf5f003d1c
NowUI <nowui@163.com>
2018-02-03 10:49:21 +0800

    修改读写分离的代码生成

--- module-shop/module-shop-api/src/main/java/com/nowui/cloud/shop/product/repository/impl/ProductRepositoryImpl.java
+++ module-shop/module-shop-api/src/main/java/com/nowui/cloud/shop/product/repository/impl/ProductRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.nowui.cloud.shop.product.repository.impl;
 import com.nowui.cloud.repository.impl.BaseRepositoryImpl;
 import com.nowui.cloud.shop.product.repository.ProductRepository;
 import com.nowui.cloud.shop.product.view.ProductView;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -15,11 +16,24 @@ public class ProductRepositoryImpl extends BaseRepositoryImpl<ProductView> imple
 
     @Override
     public Integer countForAdmin(String appId, String productName) {
-        return null;
+        Criteria criteria = Criteria.where(ProductView.APP_ID).is(appId)
+                .and(productName).regex(".*?\\" + productName + ".*")
+                .and(ProductView.SYSTEM_STATUS).is(true);
+
+        Integer count = count(criteria);
+
+        return count;
     }
 
     @Override
     public List<ProductView> listForAdmin(String appId, String productName, Integer pageIndex, Integer pageSize) {
-        return null;
+        Criteria criteria = Criteria.where(ProductView.APP_ID).is(appId)
+                .and(productName)
+                .regex(".*?\\" + productName + ".*")
+                .and(ProductView.SYSTEM_STATUS).is(true);
+
+        List<ProductView> productViewList = list(criteria, pageIndex, pageSize);
+
+        return productViewList;
     }
 }
