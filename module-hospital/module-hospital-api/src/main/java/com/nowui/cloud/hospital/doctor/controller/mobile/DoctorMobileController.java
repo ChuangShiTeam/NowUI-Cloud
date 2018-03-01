4f8bf8866badb73402051f9dd7348d2a32a646ba
NowUI <nowui@163.com>
2018-03-01 18:53:27 +0800

    新增医院服务

---
+++ module-hospital/module-hospital-api/src/main/java/com/nowui/cloud/hospital/doctor/controller/mobile/DoctorMobileController.java
@@ -0,0 +1,18 @@
+package com.nowui.cloud.hospital.doctor.controller.mobile;
+
+import com.nowui.cloud.controller.BaseController;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 医生移动端控制器
+ *
+ * @author ZhongYongQiangZ
+ *
+ * 2018-03-01
+ */
+@Api(value = "医生", description = "医生移动端接口管理")
+@RestController
+public class DoctorMobileController extends BaseController {
+
+}
---
+++ module-hospital/module-hospital-api/src/main/java/com/nowui/cloud/hospital/doctor/service/impl/DoctorServiceImpl.java
@@ -0,0 +1,63 @@
+package com.nowui.cloud.hospital.doctor.service.impl;
+
+import com.nowui.cloud.service.impl.SuperServiceImpl;
+import com.nowui.cloud.hospital.doctor.entity.Doctor;
+import com.nowui.cloud.hospital.doctor.mapper.DoctorMapper;
+import com.nowui.cloud.hospital.doctor.repository.DoctorRepository;
+import com.nowui.cloud.hospital.doctor.service.DoctorService;
+import com.nowui.cloud.hospital.doctor.view.DoctorView;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 医生业务实现
+ *
+ * @author ZhongYongQiangZ
+ *
+ * 2018-03-01
+ */
+@Service
+public class DoctorServiceImpl extends SuperServiceImpl<DoctorMapper, Doctor, DoctorRepository, DoctorView> implements DoctorService {
+
+    @Override
+        public Integer countForAdmin(String appId, String doctorName, String doctorSex, Integer doctorAge) {
+            Criteria criteria = Criteria.where(DoctorView.APP_ID).is(appId)
+                    .and(DoctorView.DOCTOR_NAME).regex(".*?" + doctorName + ".*")
+                    .and(DoctorView.DOCTOR_SEX).regex(".*?" + doctorSex + ".*")
+                    .and(DoctorView.DOCTOR_AGE).regex(".*?" + doctorAge + ".*")
+                    .and(DoctorView.SYSTEM_STATUS).is(true);
+
+            Query query = new Query(criteria);
+
+            Integer count = count(query);
+
+            return count;
+        }
+
+        @Override
+        public List<DoctorView> listForAdmin(String appId, String doctorName, String doctorSex, Integer doctorAge, Integer pageIndex, Integer pageSize) {
+            Criteria criteria = Criteria.where(Doctor.APP_ID).is(appId)
+                    .and(DoctorView.DOCTOR_NAME).regex(".*?" + doctorName + ".*")
+                    .and(DoctorView.DOCTOR_SEX).regex(".*?" + doctorSex + ".*")
+                    .and(DoctorView.DOCTOR_AGE).regex(".*?" + doctorAge + ".*")
+                    .and(DoctorView.SYSTEM_STATUS).is(true);
+
+            List<Order> orders = new ArrayList<Order>();
+            orders.add(new Order(Sort.Direction.DESC, DoctorView.SYSTEM_CREATE_TIME));
+            Sort sort = Sort.by(orders);
+
+            Query query = new Query(criteria);
+            query.with(sort);
+
+            List<DoctorView> doctorViewList = list(query, sort, pageIndex, pageSize);
+
+            return doctorViewList;
+        }
+
+}
