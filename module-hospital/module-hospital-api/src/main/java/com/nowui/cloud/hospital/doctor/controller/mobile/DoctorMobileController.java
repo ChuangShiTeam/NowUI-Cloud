6c19863b840adebeabcc1a14ae8871624962bc4e
Administrator <huangchenglin9425@163.com>
2018-03-09 16:23:05 +0800

    修改 文章分类管理 -HuangChenglin 2018年3月9日16:22:54

--- module-hospital/module-hospital-api/src/main/java/com/nowui/cloud/hospital/doctor/controller/mobile/DoctorMobileController.java
+++ module-hospital/module-hospital-api/src/main/java/com/nowui/cloud/hospital/doctor/controller/mobile/DoctorMobileController.java
@@ -1,8 +1,18 @@
 package com.nowui.cloud.hospital.doctor.controller.mobile;
 
 import com.nowui.cloud.controller.BaseController;
+import com.nowui.cloud.hospital.doctor.service.DoctorService;
+import com.nowui.cloud.hospital.doctor.view.DoctorView;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 医生移动端控制器
@@ -15,4 +25,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DoctorMobileController extends BaseController {
 
+    @Autowired
+    private DoctorService doctorService;
+
+    @ApiOperation(value = "医生列表")
+    @RequestMapping(value = "/doctor/admin/v1/list", method = {RequestMethod.POST}, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> listV1() {
+        DoctorView doctorView = getEntry(DoctorView.class);
+
+        validateRequest(
+                doctorView,
+                DoctorView.APP_ID,
+                DoctorView.DOCTOR_NAME,
+                DoctorView.DOCTOR_SEX,
+                DoctorView.DOCTOR_AGE,
+                DoctorView.PAGE_INDEX,
+                DoctorView.PAGE_SIZE
+        );
+
+        Integer resultTotal = doctorService.countForAdmin(doctorView.getAppId(), doctorView.getDoctorName(), doctorView.getDoctorSex(), doctorView.getDoctorAge());
+        List<DoctorView> resultList = doctorService.listForAdmin(doctorView.getAppId(), doctorView.getDoctorName(), doctorView.getDoctorSex(), doctorView.getDoctorAge(), doctorView.getPageIndex(), doctorView.getPageSize());
+
+        validateResponse(
+                DoctorView.DOCTOR_ID,
+                DoctorView.USER_ID,
+                DoctorView.DOCTOR_NAME,
+                DoctorView.DOCTOR_SEX,
+                DoctorView.DOCTOR_AGE,
+                DoctorView.DOCTOR_SUMMARY,
+                DoctorView.DOCTOR_PROFESSIONAL,
+                DoctorView.DOCTOR_ORGANIZATION_NAME,
+                DoctorView.DOCTOR_DEPARTMENT_NAME,
+                DoctorView.DOCTOR_MASTER,
+                DoctorView.DOCTOR_IMAGE_ID,
+                DoctorView.DOCTOR_RANK,
+                DoctorView.DOCTOR_STATUS,
+                DoctorView.DOCTOR_CERT
+        );
+
+        return renderJson(resultTotal, resultList);
+    }
 }
