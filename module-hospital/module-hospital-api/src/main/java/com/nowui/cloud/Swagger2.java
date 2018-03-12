d7df7289e0761bde9150cebb93e75fdb1256faaf
marcus <marcus_hualebin@163.com>
2018-03-12 11:00:56 +0800

    医院服务报错修改

--- module-hospital/module-hospital-api/src/main/java/com/nowui/cloud/Swagger2.java
+++ module-hospital/module-hospital-api/src/main/java/com/nowui/cloud/Swagger2.java
@@ -1,4 +1,4 @@
-package com.nowui.cloud.hospital;
+package com.nowui.cloud;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
